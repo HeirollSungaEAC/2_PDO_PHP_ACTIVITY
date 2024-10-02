c052bb918b643e20487517a848fb4a7e31deefd8
HeirollSungaEAC <167311007+HeirollSungaEAC@users.noreply.github.com>
2024-10-02 15:46:48 +0800

    Add files via upload

---
+++ 2_PDO_PHP_ACTIVITY/index.php
@@ -0,0 +1,22 @@
+<?php require_once 'core/dbConfig.php'; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>PDO_ACTIVITY</title>
+	<style>
+		table, th, td {
+		  border:1px solid black;
+		}
+	</style>
+</head>
+<>
+	
+	<?php  
+    
+	?>
+
+</body>
+</html>
